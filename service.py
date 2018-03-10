284cc9ef9c5cdbdbaa8ab34f0a3c4db756b3daa3
Tamas Hais <haist@starschema.net>
2018-03-10 14:12:14 +0100

    Adding UI: Bootstrap frontend, Flask backend ...pre-first function call ...cache predictor proto

--- service.py
+++ service.py
@@ -1,9 +1,26 @@
 from flask import Flask
 from flask import render_template
-import constants
+import constants, os
+from flask import request
+from werkzeug.contrib.cache import SimpleCache
+cache = SimpleCache()
 
 app = Flask(__name__)
 
+
+class TempObject:
+    def __init__(self):
+        self.somekey = "somevalue"
+
+@app.before_first_request
+def custom_call():
+    app.logger.debug("Starting custom call")
+
+    predictor = TempObject()
+
+    cache.set('predictor', predictor, 60*60)
+    predictor_cached = cache.get('predictor') #test if None
+    app.logger.debug(predictor_cached)
 
 @app.route('/')
 def index():
@@ -18,9 +35,18 @@ def index():
     }
     return render_template('index.html', props=building_constants)
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST',])
 def hello():
-    return 'Hello, World'
+    error = None
+    if request.method == 'POST':
+        building_levels = request.form['building_levels']
+        building_material = request.form['building_material']
+
+        pass
+    predictor = cache.get('predictor')
+
+    prediction=None
+    return render_template('prediction.html', prediction=prediction)
 
 
 
