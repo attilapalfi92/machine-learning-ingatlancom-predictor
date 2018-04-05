f76250ff69cd269ce060d07667a72bb8e28a0a0c
attilapalfi92 <attila.palfi.92@gmail.com>
2018-04-05 19:57:19 +0200

    ui fix

--- service.py
+++ service.py
@@ -26,19 +26,18 @@ def custom_call():
     #predictor_cached = cache.get('predictor') #test if None
     #app.logger.debug(predictor_cached)
 
-@app.route('/', methods=['GET',])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        building_constants = {
-            'mat': constants.BUILDING_MATERIAL,
-            'comfort': constants.COMFORT,
-            'cond': constants.CONDITION,
-            'heating': constants.HEATING,
-            'parking': constants.PARKING,
-            'sub_type': constants.SUB_TYPE,
-            'toilet': constants.TOILET_TYPE,
-        }
-        return render_template('index.html', props=building_constants)
+    building_constants = {
+        'mat': constants.BUILDING_MATERIAL,
+        'comfort': constants.COMFORT,
+        'cond': constants.CONDITION,
+        'heating': constants.HEATING,
+        'parking': constants.PARKING,
+        'sub_type': constants.SUB_TYPE,
+        'toilet': constants.TOILET_TYPE,
+    }
+    return render_template('index.html', props=building_constants)
 
 @app.route('/predict', methods=['POST',])
 def hello():
@@ -59,9 +58,9 @@ def hello():
         toilet = request.form['toilet']
 
         #TODO: debug returned type of rooms
-        if int(rooms_whole) == 0:
+        if rooms_whole == 0:
             rooms = str(rooms_half) + " fél"
-        elif int(rooms_half) == 0:
+        elif rooms_half == 0:
             rooms = str(rooms_whole)
         else:
             rooms = str(rooms_whole) + " + " + str(rooms_half) + " fél"
@@ -88,8 +87,7 @@ def hello():
         app.logger.info(prediction)
         print(str(prediction))
 
-    #return render_template('prediction.html', prediction=prediction)
-    return prediction
+    return render_template('prediction.html', prediction=prediction)
 
 
 
